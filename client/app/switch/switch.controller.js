e86cfb7b825140764eb8a930031740511eeb8656
ukiy <ukiy@raspberrypi.(none)>
2015-02-21 12:13:08 +0900

    update

--- client/app/switch/switch.controller.js
+++ client/app/switch/switch.controller.js
@@ -1,12 +1,19 @@
 'use strict';
 
 angular.module('homeAutomationApp')
-  .controller('SwitchCtrl', function ($scope) {
+  .controller('SwitchCtrl', function ($scope, $http) {
     $scope.message = 'Hello';
 
     $scope.toOn = function(){
+      $http.put('/api/gpio/4', {mode: 'out', value: '1'})
+        .success(function(data, status, header, config){
+          console.log('data', data)
+        }).error(function(data, status, header, config){
+          console.log('data', data);
+        });
     }
 
     $scope.toOff = function(){
+      $http.put('/api/gpio/4', {mode: 'out', value: '0'});
     }
   });
--- server/api/gpio/gpio.controller.js
+++ server/api/gpio/gpio.controller.js
@@ -2,7 +2,7 @@
 
 var _ = require('lodash');
 var Gpio = require('./gpio.model');
-//var gpio = require('pi-gpio');
+var gpio = require('node-pi-gpio');
 
 // Get list of gpios
 exports.index = function(req, res) {
@@ -35,16 +35,27 @@ exports.create = function(req, res) {
 
 // Updates an existing gpio in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Gpio.findById(req.params.id, function (err, gpio) {
-    if (err) { return handleError(res, err); }
-    if(!gpio) { return res.send(404); }
-    var updated = _.merge(gpio, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, gpio);
+//  if(req.body._id) { delete req.body._id; }
+//  Gpio.findById(req.params.id, function (err, gpio) {
+//    if (err) { return handleError(res, err); }
+//    if(!gpio) { return res.send(404); }
+//    var updated = _.merge(gpio, req.body);
+//    updated.save(function (err) {
+//      if (err) { return handleError(res, err); }
+//      return res.json(200, gpio);
+//    });
+//  });
+  console.log("call gpio update");
+  gpio.open(req.params.id, req.body.mode)
+    .then(function(gpio){
+      console.log(req.body.mode);
+      console.log(req.body.value);
+      gpio.value(req.body.value);
+      return res.send({mes: 'ok'});
+    }).catch(function(err){
+      console.log(err);
+      return res.send({mes: 'no'});
     });
-  });
 };
 
 // Deletes a gpio from the DB.
